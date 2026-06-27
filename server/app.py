3e3fb858dbd71531f3e73fc494bfc1967177b2f8
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-27 00:48:01 -0400

    created paginated: still needs updates for paginated

--- server/app.py
+++ server/app.py
@@ -82,9 +82,7 @@ class JournalEntryIndex(Resource):
         page = request.args.get("page", 1, type=int)
         per_page = request.args.get("per_page", 10, type=int)
 
-        paginated = JournalEntry.query.filter(
-            JournalEntry.user_id == session["user_id"]
-            ).paginate(page=page, per_page=per_page, error_out=False)
+        paginated = JournalEntry.query.filter(JournalEntry.user_id == session["user_id"]).paginate(page=page, per_page=per_page, error_out=False)
 
         return {
             "page": paginated.page,

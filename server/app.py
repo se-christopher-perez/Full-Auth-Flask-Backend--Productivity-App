a0b3591c1ae886cece3948052bdef84f64892c86
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-26 23:50:49 -0400

    began app.py andded imports

--- server/app.py
+++ server/app.py
@@ -1,3 +1,9 @@
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User, JournalEntry, UserSchema, JournalEntrySchema
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
